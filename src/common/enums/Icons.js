3941634c8d1226d3c16e3a7c4f0a151f1e455505
Amlanjyoti Saikia <amlanjyoti@91social.com>
2019-10-15 00:44:02 +0530

    Adding font assets

--- src/common/enums/Icons.js
+++ src/common/enums/Icons.js
@@ -2,6 +2,7 @@ const ICONS = {
     CALENDAR_DAY: "calendar-today",
     CALENDAR_WEEK: "calendar-week",
     CALENDAR_MONTH: "calendar-month",
+    CALENDAR_CLOCK: "calendar-clock",
     MATERIAL_COMMUNITY_ICONS: "react-native-vector-icons/MaterialCommunityIcons"
 };
 
